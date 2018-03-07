f882d064eaa4bbe2f01c03b60580aa766e16dcef
Nishad S <nishads52@gmail.com>
2018-03-07 12:00:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nishad/fetchserverdata/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.nishad.fetchserverdata;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.asyncronoustask_android_example);
+
+
+        final Button GetServerData = (Button) findViewById(R.id.GetServerData);
+
+        GetServerData.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+
+                // Server Request URL
+                String serverURL = "http://androidexample.com/media/webservice/getPage.php";
+
+                // Create Object and call AsyncTask execute Method
+                new LongOperation().execute(serverURL);
+
+            }
+        });
+
+    }
+
+
+    // Class with extends AsyncTask class
+    private class LongOperation  extends AsyncTask<String, Void, Void> {
+
+        private final HttpClient Client = new DefaultHttpClient();
+        private String Content;
+        private String Error = null;
+        private ProgressDialog Dialog = new ProgressDialog(MainActivity.this);
+        TextView uiUpdate = (TextView) findViewById(R.id.output);
+        protected void onPreExecute() {
+            // NOTE: You can call UI Element here.
+
+            //UI Element
+            uiUpdate.setText("Output : ");
+            Dialog.setMessage("Downloading source..");
+            Dialog.show();
+        }
+
+        // Call after onPreExecute method
+        protected Void doInBackground(String... urls) {
+            try {
+
+               
+                HttpGet httpget = new HttpGet(urls[0]);
+                ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                Content = Client.execute(httpget, responseHandler);
+
+            } catch (ClientProtocolException e) {
+                Error = e.getMessage();
+                cancel(true);
+            } catch (IOException e) {
+                Error = e.getMessage();
+                cancel(true);
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(Void unused) {
+            // NOTE: You can call UI Element here.
+
+            // Close progress dialog
+            Dialog.dismiss();
+
+            if (Error != null) {
+
+                uiUpdate.setText("Output : "+Error);
+
+            } else {
+
+                uiUpdate.setText("Output : "+Content);
+
+            }
+        }
+
+    }
+}
